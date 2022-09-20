b376ac555eff32f6bbad728e338aa472a0dcfcac
Matt K Hering <mhering3@student.cvtc.edu>
2022-09-19 19:13:35 -0500

    Created function inside of js file

---
+++ js/colorPicker.js
@@ -0,0 +1,8 @@
+function calculateColor() {
+
+    var red = parseFloat(document.getElementById("red").value)
+
+    //Do for Green var red = parseFloat(document.getElementById("red").value)
+
+    //Do for Blue var red = parseFloat(document.getElementById("red").value)
+}
