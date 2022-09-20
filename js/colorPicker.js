c96ea05a107a7d2c88c5788f7f9088bfa0e12352
Matt K Hering <mhering3@student.cvtc.edu>
2022-09-20 00:20:16 -0500

    [Finishes #183319926] Completed Convertion from RGB to ρɣβL.

--- js/colorPicker.js
+++ js/colorPicker.js
@@ -9,19 +9,51 @@ function convertColor() {
   if (red > 1 | red < 0) {
     alert("Make sure your number is between 0.0 and 1.0")
     console.log("Invalid number")
+
   } else if (green > 1 | green < 0) {
     alert("Make sure your number is between 0.0 and 1.0")
     console.log("Invalid number")
+
   } else if (blue > 1 | blue < 0) {
     alert("Make sure your number is between 0.0 and 1.0")
     console.log("Invalid number")
+
   } else {
     //If the number is between 0.0 and 1.0 than continue
-    alert("Nice job!")
   }
-    
-  console.log("Colors are " + red +"" + green +"" + blue)
-  document.getElementById("output").style.background = string;
+  //Create Pho, Gamma, Beta, and luminance variables
+
+  var pho
+  var gamma
+  var beta
+  var total = green + red + blue;
+
+  var luminance = Math.max(red, green, blue);
+
+//Multiply by 255
+  var rgbRed = red * 255;
+  var rgbGreen = green * 255;
+  var rgbBlue = blue * 255;
+
+
+  pho = red / total;
+  pho = red.toFixed(1);
+
+  gamma = green / total;
+  gamma = green.toFixed(1);
+
+  beta = blue / total;
+  beta = blue.toFixed(1);
+
+
+
+
+
+  document.getElementById("output").innerHTML = "(" + pho + ", " + gamma + ", " + beta + ", " + luminance + ")";
+
+  var outputColor = "rgb(" + rgbRed + "," + rgbGreen + "," + rgbBlue + ")";
+
+  document.getElementById("colorSquare").style.background = outputColor
   }
 
 
